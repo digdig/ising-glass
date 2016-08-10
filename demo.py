554a3cd5ff374e69b73a6faf49c5c57fc6ae7770
digdig <hiluming@gmail.com>
2016-08-11 02:44:58 +0800

    Add the function , change the random update to one spin.
    The two changes make the code run fast. Also changed some variable name and function name.
    
    This is the best this algorithm can do, need to think more advanced method.

--- demo.py
+++ demo.py
@@ -2,50 +2,53 @@
 import sys
 import numpy as np
 
-def evaluateEnergy(J, configuration):
+def totalEnergy(J, configuration):
     """
-    calculate the energy of a given configuration.
+    calculate the energy for a given configuration.
 
     Parameters:
         >>> J: 
         nSpin*nSpin matrix, J[i,j] is the interation between spin i and j.
         >>> configuration: 
-        nSpin*1 vector, configuration[i] stores the spin at site i.
+        1*nSpin vector, configuration[i] stores the spin at site i.
 
     Returns:
         the total energy of this configuration.
     """
+    # 0.5 for double counting
     return 0.5*np.dot(configuration,np.dot(J,configuration))
 
 
-def energyChange(J,oldConfiguration,iSite):
+def energyChange(J,configuration,randomSite):
     """
-    calculate the energy change when flip one spin at site i.
+    calculate the energy change when flip one spin at one randomSite.
 
     Parameters:
         >>> J:
             nSpin*nSpin matrix, J[i,j] is the interation between spin i and j.
-        >>> oldConfiguration:
-            the configuration before flip.
-        >>> iSite:
-            the site that the spin flips
-
+        >>> configuration:
+            the configuration before flip the spin.
+        >>> randomSite:
+            the site number that the spin flips
+    Returns:
+        the energy difference when flip one spin
     """
-    pass
+    # energy after - energy before, double counting considered
+    return -configuration[randomSite]*np.dot(J[randomSite,:],configuration)
 
 
 
-def readCoupling(nSpin):
+def getJ(nSpin):
     """
-    read spin-spin coupling strength from the file(example.txt).
+    read spin-spin coupling strength from the file (example.txt).
     
     Parameters:
         >>> nSpin:
             the number of spins, in example.txt is 300.
 
     Returns:
-        the symmetric matrix J with J[i,j] represent the coupling strength 
-        of spin[i] and spin[j].
+        the symmetric matrix J, with J[i,j] represent the coupling strength 
+        between spin[i] and spin[j].
     """
     J = np.zeros((nSpin,nSpin)) #zero if not defined in the file
     with open(sys.argv[1], 'r') as f:
@@ -60,38 +63,32 @@ def readCoupling(nSpin):
 
 def getConfiguration(nSpin, J, nFlip):
     """
-    Random flip spin and accept the configuration if the energy is lower.
+    Random flip one spin and accept the configuration if the energy is lower.
 
     Parameters:
         >>> nSpin: spin number in the system.
-        >>> nFlip: random flip spin times.
+        >>> nFlip: number of random one spin flip.
         >>> J: the coupling matrix read from the file
 
     Returns:
-        minConfiguration: the minimum energy spin configuration.
-        minEnergy: the corresponding minimum energy.
+        configuration: the minimum energy spin configuration.
+        energy: the corresponding minimum energy.
     """
-    minEnergy = 0
-    #Flip mSites' spins a time
-    mSites = 1
     #generate a random configuration first
-    newConfiguration = np.random.randint(0,2,nSpin)*2 -1
+    configuration = np.random.randint(0,2,nSpin)*2 -1
     for iFlip in range(nFlip):
-        randomSites = np.random.choice(nSpin,mSites,replace=False)
-        newConfiguration[randomSites] *= -1
-        newEnergy = evaluateEnergy(J, newConfiguration)
-        if newEnergy < minEnergy:
-            minEnergy = newEnergy
-            minConfiguration = newConfiguration
+        randomSite = np.random.choice(nSpin)
+        if energyChange(J,configuration,randomSite) < 0:
+            configuration[randomSite] *= -1
 
-    return minEnergy, minConfiguration
+    return totalEnergy(J,configuration), configuration
 
 
 
 nSpin = 300
-nFlip = nSpin*50
-J = readCoupling(nSpin)
+nFlip = nSpin*300
+J = getJ(nSpin)
 np.random.seed()
-minEnergy, minConfiguration = getConfiguration(nSpin, J, nFlip)
-print minEnergy
-print 'v '+' '.join(map(str, minConfiguration))
+energy, configuration = getConfiguration(nSpin, J, nFlip)
+print energy
+print 'v '+' '.join(map(str, configuration))
